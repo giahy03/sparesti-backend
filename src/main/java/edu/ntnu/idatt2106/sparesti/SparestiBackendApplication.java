7b623f81292e419526f569f0d48b8af9d9af209c
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-15 13:56:52 +0200

    Initial commit

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/SparestiBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2106.sparesti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparestiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparestiBackendApplication.class, args);
+	}
+
+}
