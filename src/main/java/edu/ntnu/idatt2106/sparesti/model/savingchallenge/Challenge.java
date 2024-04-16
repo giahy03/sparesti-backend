e209a69c187387269b69954c40a1571ff0f3c5ec
tabir <tabirijeffrey@gmail.com>
2024-04-16 14:08:53 +0200

    feat(challenge): create challenge interface

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/savingchallenge/Challenge.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2106.sparesti.model.savingchallenge;
+
+/**
+ * Represents a saving challenge.
+ * A saving challenge is a challenge that the user can take to
+ * save a certain amount of money in a given time frame.
+ *
+ * @author Jeffrey Yaw Annor Tabiri
+ * @version 1.0
+ */
+public interface Challenge {
+  boolean checkCompletion();
+}
