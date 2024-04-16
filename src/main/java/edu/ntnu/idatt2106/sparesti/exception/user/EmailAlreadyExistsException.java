11a0c805e18d3af796dcf349bdfabd2dbc69c2f5
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-16 11:06:35 +0200

    Create custom exception for existing email address
    Introduce EmailAlreadyExistsException class to handle scenarios where an email address is already in use by another user.

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/exception/user/EmailAlreadyExistsException.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2106.sparesti.exception.user;
+
+/**
+ * The class represents a custom exception to indicate that an email
+ * is already in use by another user.
+ * The code is inspired by Ramtin Samavat's GitHub repository: <a href="https://github.com/RamtinS/quiz-app-backend/blob/main/src/main/java/edu/ntnu/idatt2105/quizapp/exception/user/EmailAlreadyExistsException.java">...</a>
+ *
+ * @author Ramtin Samavat
+ * @version 1.0
+ */
+public class EmailAlreadyExistsException extends RuntimeException {
+
+  /**
+   * Constructs an instance of the class with the specified error message.
+   *
+   * @param message The error message.
+   */
+  public EmailAlreadyExistsException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs an instance of the clas with a predefined error message.
+   */
+  public EmailAlreadyExistsException() {
+    super("Email already in use by another user.");
+  }
+}
