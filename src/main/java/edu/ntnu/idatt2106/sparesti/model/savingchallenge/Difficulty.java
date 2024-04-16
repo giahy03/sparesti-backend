a13c23f3123e7815b9fe087a89ea2ab049f71ce9
tabir <tabirijeffrey@gmail.com>
2024-04-16 14:07:45 +0200

    feat(enum): create difficulty for challenge

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/savingchallenge/Difficulty.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2106.sparesti.model.savingchallenge;
+
+/**
+ * Represents the difficulty of a saving challenge.
+ * The difficulty is used to determine the amount
+ * of money the user has to save to complete the challenge.
+ *
+ * @author Jeffrey Yaw Annor Tabiri
+ * @version 1.0
+ */
+public enum Difficulty {
+  EASY,
+  MEDIUM,
+  HARD;
+}
+
