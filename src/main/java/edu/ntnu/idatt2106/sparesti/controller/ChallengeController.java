db51c20ed6b70f8ce3cc2add74417e4487a226f9
tabir <tabirijeffrey@gmail.com>
2024-04-16 14:17:20 +0200

    feat(controller): create challenge controller

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/controller/ChallengeController.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2106.sparesti.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Controller class for challenges.
+ * This class is responsible for handling HTTP requests related to challenges.
+ */
+@RestController
+@RequiredArgsConstructor
+public class ChallengeController {
+
+
+}
