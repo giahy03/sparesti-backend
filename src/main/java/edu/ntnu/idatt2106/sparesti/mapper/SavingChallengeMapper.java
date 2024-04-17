378e6006d3da96246fb1d3c3669a702ad64ecbef
tabir <tabirijeffrey@gmail.com>
2024-04-17 16:41:28 +0200

    fix(mapper): fix build of mapper

--- src/main/java/edu/ntnu/idatt2106/sparesti/mapper/SavingChallengeMapper.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/mapper/SavingChallengeMapper.java
@@ -4,12 +4,22 @@ import edu.ntnu.idatt2106.sparesti.dto.challenge.ChallengeDto;
 import edu.ntnu.idatt2106.sparesti.dto.challenge.SavingChallengeDto;
 import edu.ntnu.idatt2106.sparesti.mapper.ChallengeMapper;
 import edu.ntnu.idatt2106.sparesti.model.challenge.SavingChallenge;
+import edu.ntnu.idatt2106.sparesti.model.user.User;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 
+/**
+ * A mapper for mapping between SavingChallenge and SavingChallengeDto.
+ * The mapper is used to map between the two classes.
+ *
+ * @see ChallengeMapper
+ * @see SavingChallenge
+ * @author Jeffrey Yaw Annor Tabiri
+ * @version 1.0
+ */
 @Mapper(uses = ChallengeMapper.class)
 public interface SavingChallengeMapper{
-  SavingChallenge savingChallengeDtoToSavingChallenge(SavingChallengeDto challenge, @Context ChallengeMapper challengeMapperImpl);
+  SavingChallenge savingChallengeDtoToSavingChallenge(SavingChallengeDto challenge, @Context User user, @Context ChallengeMapper challengeMapperImpl);
 
   SavingChallengeDto savingChallengeDto(SavingChallenge savingChallenge, @Context ChallengeMapper challengeMapperImpl);
 }
