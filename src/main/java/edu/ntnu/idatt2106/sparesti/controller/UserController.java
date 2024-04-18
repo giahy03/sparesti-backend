5fcdbfb633d72478ff21f19ffcbda70a016db0ce
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-18 15:40:57 +0200

    feat(controller): Add retrieveUserDetails endpoint and Swagger documentation to UserController

--- src/main/java/edu/ntnu/idatt2106/sparesti/controller/UserController.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/controller/UserController.java
@@ -3,12 +3,19 @@ package edu.ntnu.idatt2106.sparesti.controller;
 import edu.ntnu.idatt2106.sparesti.dto.user.FirstNameChangeDto;
 import edu.ntnu.idatt2106.sparesti.dto.user.LastNameChangeDto;
 import edu.ntnu.idatt2106.sparesti.dto.user.PasswordChangeDto;
+import edu.ntnu.idatt2106.sparesti.dto.user.UserDetailsDto;
 import edu.ntnu.idatt2106.sparesti.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +42,13 @@ public class UserController {
    * @param principal The principal object representing the currently authenticated user.
    * @return A ResponseEntity with status OK if the operation is successful.
    */
+  @Operation(summary = "Change user password")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User password successfully updated."),
+          @ApiResponse(responseCode = "400", description = "Invalid password from user."),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
   @PutMapping(path = "/password")
   public ResponseEntity<Void> changePassword(@RequestBody PasswordChangeDto passwordChangeDto,
                                                  Principal principal) {
@@ -52,6 +66,13 @@ public class UserController {
    * @param principal The principal object representing the currently authenticated user.
    * @return A ResponseEntity with status OK if the operation is successful.
    */
+  @Operation(summary = "Change user's first name")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User fist name successfully updated."),
+          @ApiResponse(responseCode = "400", description = "Invalid first name from user."),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
   @PutMapping(path = "/first-name")
   public ResponseEntity<Void> changeFirsName(@RequestBody FirstNameChangeDto firstNameChangeDto,
                                                  Principal principal) {
@@ -69,6 +90,13 @@ public class UserController {
    * @param principal The principal object representing the currently authenticated user.
    * @return A ResponseEntity with status OK if the operation is successful.
    */
+  @Operation(summary = "Change user's last name")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User fist name successfully updated."),
+          @ApiResponse(responseCode = "400", description = "Invalid last name from user."),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
   @PutMapping(path = "/last-name")
   public ResponseEntity<Void> changeLastName(@RequestBody LastNameChangeDto lastNameChangeDto,
                                                  Principal principal) {
@@ -77,5 +105,29 @@ public class UserController {
     userService.editLastName(lastNameChangeDto, email);
     log.info("Last name for {} successfully updated.", email);
     return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  /**
+   * REST-endpoint for retrieving user information.
+   *
+   * @param principal The authenticated principal
+   * @return ResponseEntity containing user information DTO on success.
+   */
+  @Operation(summary = "Retrieve user details")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User information successfully retrieved.",
+                  content = {@Content(mediaType = "application/json",
+                          schema = @Schema(implementation = UserDetailsDto.class))}
+          ),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
+  @GetMapping(path = "/details")
+  public ResponseEntity<UserDetailsDto> retrieveUserDetails(Principal principal) {
+    String username = principal.getName();
+    log.info("Retrieving user information for user {}.", username);
+    UserDetailsDto userDetails = userService.getUserDetails(username);
+    log.info("User information for {} successfully retrieved.", username);
+    return new ResponseEntity<>(userDetails, HttpStatus.OK);
   }
 }
