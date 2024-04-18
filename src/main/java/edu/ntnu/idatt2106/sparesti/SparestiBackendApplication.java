45a1d71875d39a31c56391a34916c0d8c1c2d23f
tabir <tabirijeffrey@gmail.com>
2024-04-18 11:31:13 +0200

    chore(user): add test user in command line runner

--- src/main/java/edu/ntnu/idatt2106/sparesti/SparestiBackendApplication.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/SparestiBackendApplication.java
@@ -1,15 +1,50 @@
 package edu.ntnu.idatt2106.sparesti;
 
+import edu.ntnu.idatt2106.sparesti.model.user.Role;
+import edu.ntnu.idatt2106.sparesti.model.user.User;
+import edu.ntnu.idatt2106.sparesti.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SparestiBackendApplication {
 
+	@Autowired
+	UserRepository userRepository;
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SparestiBackendApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("Creating default user");
+
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+			User user = User.builder()
+							.email("admin@example.com")
+							.firstName("Admin")
+							.lastName("Admin")
+							.password(passwordEncoder.encode("password"))
+							.role(Role.USER)
+							.build();
+
+			userRepository.save(user);
+		};
+
+
+	}
+
 
 }
