9079ec611c54452316f74ce08384df3d7017824a
tabir <tabirijeffrey@gmail.com>
2024-04-18 10:08:32 +0200

    feat(Email): add email code entity

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/EmailCode.java
@@ -0,0 +1,38 @@
+package edu.ntnu.idatt2106.sparesti.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class EmailCode {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "email_id")
+  @Setter(AccessLevel.NONE)
+  private Long id;
+
+  @Column(name = "register_email", nullable = false, unique = true)
+  @NonNull
+  private String registerEmail;
+
+  @Column(name = "verification_code", nullable = false)
+  @NonNull
+  private String verificationCode;
+}
