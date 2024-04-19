517edc696edf68d0cae4635eba4bc43226f726fb
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-19 13:29:27 +0200

    feat(repository): Add UserInfoRepository interface for managing UserInfo entities

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/repositories/user/UserInfoRepository.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2106.sparesti.repositories.user;
+
+import edu.ntnu.idatt2106.sparesti.model.user.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface for managing UserInfo entities.
+ * The interface extends JpaRepository, allowing basic CRUD
+ * (Create, Read, Update, Delete) operations on the entities.
+ *
+ * @author Ramtin Samavat
+ * @version 1.0
+ */
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+}
