b63bce18d7d8144c158e6f4c79fbfc1ba7c6746d
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-19 09:59:33 +0200

    feat(controller): Add REST endpoints for changing income and living status in UserController

--- src/main/java/edu/ntnu/idatt2106/sparesti/controller/UserController.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/controller/UserController.java
@@ -1,9 +1,10 @@
 package edu.ntnu.idatt2106.sparesti.controller;
 
+import edu.ntnu.idatt2106.sparesti.dto.user.UserDetailsDto;
 import edu.ntnu.idatt2106.sparesti.dto.user.edit.FirstNameChangeDto;
+import edu.ntnu.idatt2106.sparesti.dto.user.edit.IncomeChangeDto;
 import edu.ntnu.idatt2106.sparesti.dto.user.edit.LastNameChangeDto;
 import edu.ntnu.idatt2106.sparesti.dto.user.edit.PasswordChangeDto;
-import edu.ntnu.idatt2106.sparesti.dto.user.UserDetailsDto;
 import edu.ntnu.idatt2106.sparesti.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -36,7 +37,7 @@ public class UserController {
   private final UserService userService;
 
   /**
-   * Changes the password of the currently logged-in user.
+   * REST-endpoint for changing the password of the currently logged-in user.
    *
    * @param passwordChangeDto The DTO containing old and new password.
    * @param principal The principal object representing the currently authenticated user.
@@ -60,7 +61,7 @@ public class UserController {
   }
 
   /**
-   * Changes the first name of the currently logged-in user.
+   * REST-endpoint for changing the first name of the currently logged-in user.
    *
    * @param firstNameChangeDto The DTO containing the new first name.
    * @param principal The principal object representing the currently authenticated user.
@@ -84,7 +85,7 @@ public class UserController {
   }
 
   /**
-   * Changes the last name of the currently logged-in user.
+   * REST-endpoint for changing the last name of the currently logged-in user.
    *
    * @param lastNameChangeDto The DTO containing the new last name.
    * @param principal The principal object representing the currently authenticated user.
@@ -92,7 +93,7 @@ public class UserController {
    */
   @Operation(summary = "Change user's last name")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "User fist name successfully updated."),
+          @ApiResponse(responseCode = "200", description = "User last name successfully updated."),
           @ApiResponse(responseCode = "400", description = "Invalid last name from user."),
           @ApiResponse(responseCode = "500", description = "Internal server error.",
                   content = @Content)
@@ -108,6 +109,52 @@ public class UserController {
   }
 
   /**
+   * REST-endpoint for changing the income of the currently logged-in user.
+   *
+   * @param incomeChangeDto The DTO containing the new income.
+   * @param principal The principal object representing the currently authenticated user.
+   * @return A ResponseEntity with status OK if the operation is successful.
+   */
+  @Operation(summary = "Change user's income")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User income successfully updated."),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
+  @PutMapping(path = "/income")
+  public ResponseEntity<Void> changeIncome(@RequestBody IncomeChangeDto incomeChangeDto,
+                                             Principal principal) {
+    String email = principal.getName();
+    log.info("Changing income for user with email {}.", email);
+    userService.editIncome(incomeChangeDto, email);
+    log.info("Income for {} successfully updated.", email);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  /**
+   * REST-endpoint for changing the living status of the currently logged-in user.
+   *
+   * @param incomeChangeDto The DTO containing the new living status.
+   * @param principal The principal object representing the currently authenticated user.
+   * @return A ResponseEntity with status OK if the operation is successful.
+   */
+  @Operation(summary = "Change user's living status")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "User fist name successfully updated."),
+          @ApiResponse(responseCode = "500", description = "Internal server error.",
+                  content = @Content)
+  })
+  @PutMapping(path = "/living-status")
+  public ResponseEntity<Void> changeLivingStatus(@RequestBody IncomeChangeDto incomeChangeDto,
+                                           Principal principal) {
+    String email = principal.getName();
+    log.info("Changing living status for user with email {}.", email);
+    userService.editIncome(incomeChangeDto, email);
+    log.info("Living status for {} successfully updated.", email);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  /**
    * REST-endpoint for retrieving user information.
    *
    * @param principal The authenticated principal
@@ -115,7 +162,8 @@ public class UserController {
    */
   @Operation(summary = "Retrieve user details")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "User information successfully retrieved.",
+          @ApiResponse(responseCode = "200",
+                  description = "User information successfully retrieved.",
                   content = {@Content(mediaType = "application/json",
                           schema = @Schema(implementation = UserDetailsDto.class))}
           ),
