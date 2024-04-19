ad9ebff3fe50aca0cb96302cccf0ab57e81e697a
olavsro <olavsro@stud.ntnu.no>
2024-04-19 12:14:50 +0200

    Added get news functionality

--- src/main/java/edu/ntnu/idatt2106/sparesti/dto/NewsDto.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/dto/NewsDto.java
@@ -1,7 +1,9 @@
 package edu.ntnu.idatt2106.sparesti.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Data Transfer Object (DTO) containing news objects.
@@ -10,6 +12,8 @@ import lombok.NonNull;
  * @version 1.0
  */
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class NewsDto {
   @NonNull
   private String title;
--- src/main/java/edu/ntnu/idatt2106/sparesti/service/news/NewsScrapingService.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/service/news/NewsScrapingService.java
@@ -4,15 +4,23 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
+import org.springframework.stereotype.Service;
 
+import edu.ntnu.idatt2106.sparesti.dto.NewsDto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class NewsScrapingService {
 
-    public void scrapeDN() {
+
+    public List<NewsDto> scrapeDN() {
+        List<NewsDto> newsList = new ArrayList<>();
         try {
             String url = "https://www.dn.no";
             Document doc = Jsoup.connect(url).get(); 
-
             Elements newsArticles = doc.select("article[data-articleid]"); 
 
             for (Element article : newsArticles) {
@@ -21,14 +29,17 @@ public class NewsScrapingService {
                 String articleUrl = url + article.select(".dre-item__asset").attr("href"); 
                 String category = article.select(".dre-item__footer a").text();
 
+                NewsDto newsDto = new NewsDto();
+                newsDto.setTitle(title);
+                newsDto.setImageUrl(imageUrl);
+                newsDto.setArticleUrl(articleUrl);
+                newsDto.setCategory(category);
+
+                newsList.add(newsDto);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        NewsScrapingService service = new NewsScrapingService();
-        service.scrapeDN();
+        return newsList; // Consider also what to return or how to handle errors if the try block fails
     }
 }
