72f248cdf483eda49db31ec213bdd59413942fcd
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-22 10:56:23 +0200

    refactor(dto): Remove attachment attribute from EmailDetailsDto

--- src/main/java/edu/ntnu/idatt2106/sparesti/dto/email/EmailDetailsDto.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/dto/email/EmailDetailsDto.java
@@ -1,26 +1,16 @@
 package edu.ntnu.idatt2106.sparesti.dto.email;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import lombok.Data;
 
 /**
- * Represents certain information about an email.
+ * DTO that represents certain information about an email.
  *
  * @author Jeffrey Yaw Annor Tabiri
  * @version 1.0
  */
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class EmailDetailsDto {
   private String recipient;
   private String subject;
   private String body;
-  private String attachment;
 }
