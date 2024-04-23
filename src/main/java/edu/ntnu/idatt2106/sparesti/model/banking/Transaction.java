6045ab2b0bf938eaa20c44f6ceb963df9168a40e
Tobias <oftedal03@gmail.com>
2024-04-23 10:43:27 +0200

    feature(analysis) make sql table for Transaction

--- src/main/java/edu/ntnu/idatt2106/sparesti/model/banking/Transaction.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/banking/Transaction.java
@@ -1,17 +1,41 @@
 package edu.ntnu.idatt2106.sparesti.model.banking;
 
 import edu.ntnu.idatt2106.sparesti.model.analysis.ssb.SsbPurchaseCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.MonthDay;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * Class representing a transaction in a bank statement.
  */
-@Data
+@Getter
+@Setter
 @RequiredArgsConstructor
+@Table(name = "transactions")
+@Entity
+@NoArgsConstructor
 public class Transaction {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(description = "The unique identifier for the user")
+  @Column(name = "id")
+  @Setter(AccessLevel.NONE)
+  private Long id;
+
+
   @NonNull
   private MonthDay date;
   @NonNull
@@ -21,5 +45,8 @@ public class Transaction {
   @NonNull
   private Boolean isIncoming;
   private SsbPurchaseCategory category;
+
+  @ManyToOne
+  private BankStatement bankStatement;
 }
 
