cacc09563be2b66fcf6a113ab202e3f04c522b28
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-23 11:50:30 +0200

    feat(dto): Create EmailCodeExpirationDto for sending the expiration time of the email code

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/dto/email/EmailCodeExpirationDto.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2106.sparesti.dto.email;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NonNull;
+
+/**
+ * Data Transfer Object (DTO) containing email code with expiration time.
+ *
+ * @author Ramtin Samavat
+ * @version 1.0
+ */
+@Data
+public class EmailCodeExpirationDto {
+  @NonNull
+  public LocalDateTime expirationTimestamp;
+}
