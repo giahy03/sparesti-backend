62d42911b287549e705685e3ef7103fac085f1f0
Tobias <oftedal03@gmail.com>
2024-04-23 10:40:19 +0200

    feature(analysis) make sql table for BankStatementAnalysis

--- src/main/java/edu/ntnu/idatt2106/sparesti/model/analysis/BankStatementAnalysis.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/analysis/BankStatementAnalysis.java
@@ -1,16 +1,44 @@
 package edu.ntnu.idatt2106.sparesti.model.analysis;
 
+import edu.ntnu.idatt2106.sparesti.model.banking.BankStatement;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * Represents the analysis of a bank statement.
  */
-@Data
+@Getter
+@Setter
+@Table(name = "bank_statement_analyses")
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class BankStatementAnalysis {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
   @NonNull
-  List<AnalysisItem> analysisItems;
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "bankStatementAnalysis")
+  private List<AnalysisItem> analysisItems;
+
+  @OneToOne(cascade = CascadeType.ALL, mappedBy = "analysis")
+  private BankStatement bankStatement;
 
   @Override
   public String toString() {
