d77ad99380891768b5fd44a00f1c2cc735c60865
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-23 13:28:16 +0200

    feat(dto): Add new attributes income and livingStatus to UserDetailsDto

--- src/main/java/edu/ntnu/idatt2106/sparesti/dto/user/UserDetailsDto.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/dto/user/UserDetailsDto.java
@@ -1,19 +1,27 @@
 package edu.ntnu.idatt2106.sparesti.dto.user;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
 /**
- * Data Transfer Object (DTO) representing user details.
+ * Data Transfer Object (DTO) representing user information.
  *
  * @author Ramtin Samavat
  * @version 1.0
  */
 @Data
+@Builder
 public class UserDetailsDto {
+
   @NonNull
   private String firstName;
 
   @NonNull
   private String lastName;
+
+  private double income;
+
+  @NonNull
+  private String livingStatus;;
 }
