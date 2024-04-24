4c03a89d2466bf11cfd0be4985bbaa9a03551031
tabir <tabirijeffrey@gmail.com>
2024-04-24 14:11:46 +0200

    refactor(Challenge): update controller

--- src/main/java/edu/ntnu/idatt2106/sparesti/controller/ChallengeController.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/controller/ChallengeController.java
@@ -4,14 +4,11 @@ import edu.ntnu.idatt2106.sparesti.dto.challenge.ChallengeDto;
 import edu.ntnu.idatt2106.sparesti.dto.challenge.ChallengePreviewDto;
 import edu.ntnu.idatt2106.sparesti.dto.challenge.ChallengeUpdateRequestDto;
 import edu.ntnu.idatt2106.sparesti.service.challenge.ChallengeService;
-import java.security.Principal;
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -26,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 /**
@@ -43,90 +41,133 @@ public class ChallengeController {
 
   private final ChallengeService challengeService;
 
+  /**
+   * Get all challenges to the given user.
+   *
+   * @param principal the user that wants to get the challenges.
+   * @param page the page number to get.
+   * @param pageSize the number of challenges to get.
+   * @return ResponseEntity containing the challenges.
+   */
   @Operation(summary = "Get all challenges to the given user.")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "The challenges were found and "
-                  + "returned", content = {
-                  @Content(mediaType = "application/json", schema = @Schema(implementation =
-                          ChallengePreviewDto.class))})})
+      @ApiResponse(responseCode = "200",
+              description = "The challenges were found and " + "returned")})
   @GetMapping("/challenges")
-  public ResponseEntity<List<ChallengePreviewDto>> getChallengesForUser(Principal principal,
-                                                     @RequestParam int page,
-                                                     @RequestParam int pageSize) {
+  public ResponseEntity<List<ChallengePreviewDto>> getChallengesForUser(
+          Principal principal,
+          @RequestParam int page,
+          @RequestParam int pageSize) {
 
     log.info("Getting challenges for user: {}", principal.getName());
     PageRequest pageRequest = PageRequest.of(page, pageSize);
     List<ChallengePreviewDto> challenges = challengeService.getChallenges(principal, pageRequest);
 
+    log.info("Challenges successfully retrieved for user: {}", principal.getName());
     return new ResponseEntity<>(challenges, HttpStatus.OK);
   }
 
+  /**
+   * Add a challenge to the given user.
+   *
+   * @param principal the user that wants to add the challenge.
+   * @param challenge the challenge to add.
+   * @return ResponseEntity containing the status of the operation.
+   */
   @Operation(summary = "Add a challenge to the given user.")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "The challenge was added successfully"),
-          @ApiResponse(responseCode = "400", description = "The challenge could not be added")
+      @ApiResponse(responseCode = "200", description = "The challenge was added successfully"),
+      @ApiResponse(responseCode = "400", description = "The challenge could not be added")
   })
   @PostMapping("/challenge")
   public ResponseEntity<String> addChallenge(Principal principal,
                                              @RequestBody ChallengeDto challenge) {
 
+    log.info("Adding challenge for user: {}", principal.getName());
     challengeService.addChallenge(principal, challenge);
 
-    return new ResponseEntity<>("OK", HttpStatus.OK);
+    log.info("Challenge successfully added for user: {}", principal.getName());
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
 
+  /**
+   * Get a specific challenge to the given user.
+   *
+   * @param principal the user that wants to get the challenge.
+   * @param challengeId the id of the challenge to get.
+   * @return ResponseEntity containing the challenge.
+   */
   @Operation(summary = "Get a specific challenge to the given user.")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "The challenge was added successfully"),
-          @ApiResponse(responseCode = "400", description = "The challenge could not be found"),
-          @ApiResponse(responseCode = "401", description = "The user is not authorized to change the challenge")
+      @ApiResponse(responseCode = "200",
+              description = "The challenge was added successfully"),
+      @ApiResponse(responseCode = "400",
+              description = "The challenge could not be found"),
+      @ApiResponse(responseCode = "401",
+              description = "The user is not authorized to change the challenge"),
+      @ApiResponse(responseCode = "404",
+              description = "The user or object could not be found.")
   })
   @GetMapping("/challenge/{challengeId}")
   public ResponseEntity<ChallengeDto> getChallengeById(Principal principal,
                                                        @PathVariable long challengeId) {
 
     log.info("Getting challenge for user: {}", principal.getName());
-
     ChallengeDto challenge = challengeService.getChallenge(principal, challengeId);
 
+    log.info("Challenge successfully retrieved with id {}", challengeId);
     return new ResponseEntity<>(challenge, HttpStatus.OK);
   }
 
   /**
+   * Remove a challenge from the given user.
    *
-   * @param principal
-   * @param challengeId
-   * @return
+   * @param principal the user that wants to remove the challenge.
+   * @param challengeId the id of the challenge to remove.
+   * @return ResponseEntity containing the status of the operation.
    */
   @Operation(summary = "Remove a challenge from the given user.")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "The challenge was removed successfully"),
-          @ApiResponse(responseCode = "400", description = "The challenge could not be removed"),
-          @ApiResponse(responseCode = "401", description = "The user is not authorized to change the challenge")
+      @ApiResponse(responseCode = "200",
+              description = "The challenge was removed successfully"),
+      @ApiResponse(responseCode = "400",
+              description = "The challenge could not be removed"),
+      @ApiResponse(responseCode = "401",
+              description = "The user is not authorized to change the challenge"),
+      @ApiResponse(responseCode = "404",
+              description = "The user or object could not be found.")
   })
   @DeleteMapping("/challenge/{challengeId}")
   public ResponseEntity<String> removeChallenge(Principal principal,
                                                 @PathVariable long challengeId) {
+
     log.info("Deleting challenge for user: {}", principal.getName());
     challengeService.removeChallenge(principal, challengeId);
-    return new ResponseEntity<>("OK", HttpStatus.OK);
+
+    log.info("Challenge successfully deleted with id {}", challengeId);
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
 
   /**
    * Update a challenge for the given user.
    *
-   * @param principal The user that wants to update the challenge.
-   * @param challengeId The id of the challenge to update.
-   * @param updateRequestDto The new challenge data.
+   * @param principal        the user that wants to update the challenge.
+   * @param challengeId      the id of the challenge to update.
+   * @param updateRequestDto the new challenge data.
    * @return ResponseEntity containing the status of the operation.
    */
   @Operation(summary = "Update a challenge for the given user.")
   @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description = "The challenge was updated successfully."),
-          @ApiResponse(responseCode = "400", description = "The challenge could not be updated."),
-          @ApiResponse(responseCode = "401", description = "The user is not authorized to change the challenge")
+      @ApiResponse(responseCode = "200",
+              description = "The challenge was updated successfully."),
+      @ApiResponse(responseCode = "400",
+              description = "The challenge could not be updated."),
+          @ApiResponse(responseCode = "401",
+              description = "The user is not authorized to change the challenge."),
+      @ApiResponse(responseCode = "404",
+              description = "The user could not be found.")
   })
   @PutMapping("/challenge/{challengeId}")
   public ResponseEntity<String> updateChallenge(
@@ -134,11 +175,11 @@ public class ChallengeController {
           @PathVariable long challengeId,
           @RequestBody ChallengeUpdateRequestDto updateRequestDto) {
 
-    log.info("Updating challenge for user: {}", principal.getName());
-
+    log.info("Updating challenge {} for user: {}", challengeId, principal.getName());
     challengeService.updateChallenge(principal, challengeId, updateRequestDto);
 
-    return new ResponseEntity<>("OK", HttpStatus.OK);
+    log.info("Challenge successfully updated with id {}", challengeId);
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
 }
