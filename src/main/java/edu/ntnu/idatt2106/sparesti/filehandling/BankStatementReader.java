21c63ce321c12709b708d08f51f0db77fc2dcb65
Tobias <oftedal03@gmail.com>
2024-04-25 08:45:00 +0200

    fix(analysis) add abstract methods to BankStatementReader

--- src/main/java/edu/ntnu/idatt2106/sparesti/filehandling/BankStatementReader.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/filehandling/BankStatementReader.java
@@ -1,22 +1,50 @@
 package edu.ntnu.idatt2106.sparesti.filehandling;
 
 import edu.ntnu.idatt2106.sparesti.model.banking.BankStatement;
-import edu.ntnu.idatt2106.sparesti.model.banking.Transaction;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.data.util.Pair;
 
 /**
  * Class for reading bank statements from different banks.
  */
 @Slf4j
 public abstract class BankStatementReader {
-  public abstract BankStatement readStatement(File file);
+  public abstract void readFirstPage(String pageText, BankStatement bankStatement);
+  public abstract void readStandardPage(String pageText, BankStatement bankStatement);
+  /**
+   * Reads a bank statement from SpareBank1. And creates a {@link BankStatement} object.
+   *
+   * @param file The file to read.
+   * @return The parsed bank statement.
+   */
+  public BankStatement readStatement(File file) {
+
+    try (PDDocument document = Loader.loadPDF(file)) {
+      BankStatement bankStatement = new BankStatement();
+      bankStatement.setTransactions(new ArrayList<>());
+
+      String firstPageText = readPageToText(1, file);
+      readFirstPage(firstPageText, bankStatement);
+
+      for (int i = 2; i < document.getNumberOfPages() + 1; i++) {
+        String pageText = readPageToText(i, file);
+        readStandardPage(pageText, bankStatement);
+      }
+
+      log.info("finalised reading SpareBank1 statement from file:" + file.getName());
+      return bankStatement;
+    } catch (Exception e) {
+      log.error("Error reading SpareBank1 statement from file:" + file.toString(), e);
+      throw new IllegalArgumentException("Error reading SpareBank1 statement from file:" + file);
+    }
+  }
 
   /**
    * Just for testing purposes. Should not be used in production.
@@ -25,9 +53,7 @@ public abstract class BankStatementReader {
    */
   public void logStatementFully(Path fileLocation) {
     try (PDDocument document = Loader.loadPDF(new File(fileLocation.toString()))) {
-      log.info("Reading SpareBank1 statement from file:" + fileLocation.toString());
-      List<Transaction> transactions = new ArrayList<>();
-
+      log.info("Reading statement from file: " + fileLocation);
       String text = new PDFTextStripper().getText(document);
       String[] splitText = text.split("\n");
       for (int i = 0; i < splitText.length; i++) {
@@ -36,5 +62,45 @@ public abstract class BankStatementReader {
     } catch (Exception e) {
       log.error("Error reading SpareBank1 statement from file:" + fileLocation.toString(), e);
     }
+  }
+
+  //1-based indexing
+  public String readPageToText(int pageNumber, File file){
+    try (PDDocument document = Loader.loadPDF(file)) {
+
+      BankStatement bankStatement = new BankStatement();
+      bankStatement.setTransactions(new ArrayList<>());
+
+      PDFTextStripper stripper = new PDFTextStripper();
+      stripper.setStartPage(pageNumber);
+      stripper.setEndPage(pageNumber);
+      return stripper.getText(document)
+          .replaceAll("\t", "\n");
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Invalid pdf file");
+    }
+  }
+
+
+  /**
+   * Skips lines until it finds a line containing the textToContain.
+   *
+   * @param textToContain The text segment to find.
+   * @param splitText     The text to search through.
+   * @param lineIndex     The current index of the splitText.
+   * @return A pair containing the found line and the index of the next line. If the line is not
+   * found the first element of the pair will be empty.
+   */
+  protected Pair<Optional<String>, Integer> skipUntilFind(String textToContain, String[] splitText,
+                                                int lineIndex) {
+    String foundLine = null;
+    for (; lineIndex < splitText.length; lineIndex++) {
+      if (splitText[lineIndex].toLowerCase().contains(textToContain)) {
+        foundLine = splitText[lineIndex];
+        lineIndex = lineIndex + 1;
+        break;
+      }
+    }
+    return Pair.of(Optional.ofNullable(foundLine), lineIndex);
   }
 }
--- src/main/java/edu/ntnu/idatt2106/sparesti/filehandling/DnbReader.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/filehandling/DnbReader.java
@@ -1,16 +1,116 @@
 package edu.ntnu.idatt2106.sparesti.filehandling;
 
 import edu.ntnu.idatt2106.sparesti.model.banking.BankStatement;
-import java.io.File;
+import edu.ntnu.idatt2106.sparesti.model.banking.Transaction;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 
 /**
  * Reader for HandelsBanken bank statements.
  */
 @Slf4j
 public class DnbReader extends BankStatementReader {
+  BankStatement readStatementFromPath(Path fileLocation) {
+    log.info("Reading SpareBank1 statement from file:" + fileLocation.toString());
+
+    return readStatement(fileLocation.toFile());
+  }
+
   @Override
-  public BankStatement readStatement(File fileLocation) {
-    throw new IllegalArgumentException("Method not implemented yet");
+  public void readFirstPage(String pageText, BankStatement bankStatement) {
+
+    String[] splitText = pageText.split("\n");
+
+    int lineIndex = 0;
+    Pair<Optional<String>, Integer> accountLine =
+        skipUntilFind("kontoutskrift", splitText, lineIndex);
+
+    if (accountLine.getFirst().isEmpty()) {
+      throw new IllegalArgumentException("Could not find account number in DNB statement");
+    }
+    bankStatement.setAccountNumber(accountLine.getFirst().get().split("\\s+")[2]);
+    lineIndex = accountLine.getSecond();
+
+
+    skipUntilFind("bruk", splitText, lineIndex);
+
+
+    String currentString = "";
+    for (; lineIndex < splitText.length; lineIndex++) {
+      String line = splitText[lineIndex];
+      currentString = currentString + " " +line;
+
+      try {
+        parseTransaction(currentString, false);
+        System.out.println(currentString + " was a valid transaction");
+        currentString = "";
+      } catch (Exception e) {
+        log.info("could not parse: " + e.getMessage());
+      }
+
+    }
+
+  }
+
+  @Override
+  public void readStandardPage(String pageText, BankStatement bankStatement) {
+//    if (0 == 0){
+//      return;
+//    }
+//    String[] splitText = pageText.split("\n");
+//
+//    for (String s : splitText) {
+//
+//      String[] splitLine = s.split(" ");
+//
+//      try {
+//        Integer.parseInt(splitLine[splitLine.length - 1]);
+//        Transaction transaction;
+//        if (s.toLowerCase().contains("fra")) {
+//          transaction = parseTransaction(s, true);
+//        } else {
+//          transaction = parseTransaction(s, false);
+//        }
+//        bankStatement.getTransactions().add(transaction);
+//      } catch (Exception e) {
+//        log.info("Error while reading standard page: " + e.getMessage());
+//      }
+//    }
+  }
+
+
+  /**
+   * Parses a transaction from a line in the SpareBank1 pdf.
+   *
+   * @param line     The line to parse.
+   * @param incoming Whether the transaction is incoming or outgoing.
+   * @return The parsed transaction.
+   */
+  private Transaction parseTransaction(String line, boolean incoming) {
+    log.info("Parsing transaction: " + line);
+    String[] splitLine = line.split(" ");
+
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
+
+    String purchaseDateString = splitLine[0];
+    String bookKeepingDateString = splitLine[1];
+
+    LocalDate purchaseDate = LocalDate.parse(purchaseDateString, formatter);
+    LocalDate bookKeepingDate = LocalDate.parse(bookKeepingDateString, formatter);
+
+
+    String dateLine = splitLine[1];
+    String transactionSum = splitLine[splitLine.length - 3];
+    Integer archiveReference = Integer.parseInt(splitLine[splitLine.length - 1]);
+
+
+    return null;
   }
 }
