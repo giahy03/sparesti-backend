bfebac3d1888e83f6d3922036b0b71d0c073fdb3
Elementum <tabirijeffrey@gmail.com>
2024-04-28 16:51:21 +0200

    feat(Challenge): add shared challenge code

--- src/main/java/edu/ntnu/idatt2106/sparesti/model/challenge/SharedChallengeCode.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/challenge/SharedChallengeCode.java
@@ -1,35 +1,50 @@
 package edu.ntnu.idatt2106.sparesti.model.challenge;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
+
+/**
+ * Represents a shared challenge code.
+ *
+ * @version 1.0
+ * @author Jeffrey Yaw Annor Tabiri
+ */
 @Entity
 @Table(name = "shared_challenge_code")
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class SharedChallengeCode {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  @Getter(AccessLevel.NONE)
+  @Column(name = "shared_id")
+  @Setter(AccessLevel.NONE)
   private Long id;
 
-  @Column(name = "verification_code", nullable = false)
-  @Schema(description = "The verification code.")
+  @Column(name = "join_code", nullable = false, unique = true)
+  @Schema(description = "The join code")
   @Setter(AccessLevel.NONE)
-  @NonNull
   private String joinCode;
 
+  @OneToMany(mappedBy = "sharedChallengeCode")
+  private List<SharedChallenge> sharedChallenges;
 }
