8215c3e774e32defcf6694a0b718f8d86de168b5
tabir <tabirijeffrey@gmail.com>
2024-04-29 14:07:10 +0200

    chore: optimize imports

--- src/main/java/edu/ntnu/idatt2106/sparesti/SparestiBackendApplication.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/SparestiBackendApplication.java
@@ -1,19 +1,7 @@
 package edu.ntnu.idatt2106.sparesti;
 
-import edu.ntnu.idatt2106.sparesti.model.analysis.ssb.SsbLivingStatus;
-import edu.ntnu.idatt2106.sparesti.model.user.Role;
-import edu.ntnu.idatt2106.sparesti.model.user.User;
-import edu.ntnu.idatt2106.sparesti.model.user.UserInfo;
-import edu.ntnu.idatt2106.sparesti.repository.BadgeRepository;
-import edu.ntnu.idatt2106.sparesti.repository.user.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SparestiBackendApplication {
