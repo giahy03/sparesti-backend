909c7a4faafce16970565186bbd7c92712d7fd95
tabir <tabirijeffrey@gmail.com>
2024-04-29 10:25:14 +0200

    refactor(Challenge): add exception handling to service

---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/service/challenge/CodeGenerationUtility.java
@@ -0,0 +1,34 @@
+package edu.ntnu.idatt2106.sparesti.service.challenge;
+
+import java.security.SecureRandom;
+
+/**
+ * Utility class for generating codes for various operations.
+ *
+ * @version 1.0
+ * @author Jeffrey Yaw Annor Tabiri
+ */
+public class CodeGenerationUtility {
+  private static final String CHARACTERS =
+          """
+          abcdefghijklmnopqrstuvwxyz
+          ABCDEFGHIJKLMNOPQRSTUVWXYZ
+          0123456789
+          """;
+  private static final int LENGTH = 8;
+  private static final SecureRandom random = new SecureRandom();
+
+  /**
+   * Generates a verification token.
+   * The token is a random string of length 6.
+   *
+   * @return the generated token.
+   */
+  public static String generateJoinCode() {
+    StringBuilder token = new StringBuilder(LENGTH);
+    for (int i = 0; i < LENGTH; i++) {
+      token.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+    }
+    return token.toString();
+  }
+}
