d9d729903cd989e5650cf852a9a09cd5195962f0
H-Soreide <hanne.sofie.mss@gmail.com>
2024-04-29 15:48:19 +0200

    fix(SavingGoalRepository): Fix import statements in Saving goal repository

--- src/main/java/edu/ntnu/idatt2106/sparesti/repository/SavingGoalRepository.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/repository/SavingGoalRepository.java
@@ -3,6 +3,9 @@ package edu.ntnu.idatt2106.sparesti.repository;
 import edu.ntnu.idatt2106.sparesti.model.savingGoal.SavingGoal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 
 import java.util.Optional;
 
@@ -17,10 +20,10 @@ import java.util.Optional;
 public interface SavingGoalRepository extends JpaRepository<SavingGoal, Long> {
     Optional<SavingGoal> findById(long id);
 
-   // List<SavingGoal> findAllByUser_Username(String email, Pageable pageable);
+    List<SavingGoal> findAllByUser_Username(String email, Pageable pageable);
 
     Optional<SavingGoal> findByJoinCode(String code);
 
-    List<SavingGoal> findAllByUser_Username_AndAchievedIsTrue(String email, Pageable pageable);
+    //List<SavingGoal> findAllByUser_Username_AndAchievedIsTrue(String email, Pageable pageable);
 
 }
