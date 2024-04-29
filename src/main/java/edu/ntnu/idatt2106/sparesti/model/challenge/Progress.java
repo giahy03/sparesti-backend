dfd97879430336002ca94fbc1594d2d7c2ffa34c
tabir <tabirijeffrey@gmail.com>
2024-04-29 10:25:37 +0200

    docs: add docs to enum class

--- src/main/java/edu/ntnu/idatt2106/sparesti/model/challenge/Progress.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/challenge/Progress.java
@@ -1,5 +1,13 @@
 package edu.ntnu.idatt2106.sparesti.model.challenge;
 
+/**
+ * Represents the progress of a challenge.
+ * The progress is used to determine the status
+ * of the challenge.
+ *
+ * @version 1.0
+ * @author Jeffrey Yaw Annor Tabiri
+ */
 public enum Progress {
   NOT_STARTED,
   IN_PROGRESS,
