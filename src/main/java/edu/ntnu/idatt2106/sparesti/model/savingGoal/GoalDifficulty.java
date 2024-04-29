d6b4f87b9d792c6b6c0687fc792dcce452d3a57e
H-Soreide <hanne.sofie.mss@gmail.com>
2024-04-29 08:55:22 +0200

    refactor(savingGoal): Replace difficulty enum  with state enum

--- src/main/java/edu/ntnu/idatt2106/sparesti/model/savingGoal/GoalDifficulty.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idatt2106.sparesti.model.savingGoal;
-
-public enum GoalDifficulty {
-    EASY,
-    MEDIUM,
-    HARD;
-}
---
+++ src/main/java/edu/ntnu/idatt2106/sparesti/model/savingGoal/goalState.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idatt2106.sparesti.model.savingGoal;
+
+public enum goalState {
+    FINISHED,
+    UNDER_PROGRESS,
+    FAILED;
+}
