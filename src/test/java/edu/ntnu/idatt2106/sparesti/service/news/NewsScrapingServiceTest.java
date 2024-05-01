f9f414abd980362d6ef1c0c6edeba79dc312345e
Elementum <tabirijeffrey@gmail.com>
2024-05-02 01:57:23 +0200

    test(service): added mockito tests to service

---
+++ src/test/java/edu/ntnu/idatt2106/sparesti/service/news/NewsScrapingServiceTest.java
@@ -0,0 +1,68 @@
+package edu.ntnu.idatt2106.sparesti.service.news;
+
+import edu.ntnu.idatt2106.sparesti.dto.NewsDto;
+import java.io.IOException;
+import java.util.List;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test class for NewsScrapingService.
+ *
+ * @version 1.0
+ * @author Jeffrey Yaw Annor Tabiri
+ */
+@ExtendWith(MockitoExtension.class)
+class NewsScrapingServiceTest {
+
+  @InjectMocks
+  private NewsScrapingService newsScrapingService;
+
+  @Mock
+  Document doc;
+  @Mock
+  Elements elements;
+
+  @Test
+  void Service_ScrapeDN_ReturnsNewsDtoList() throws IOException {
+
+    // Arrange
+    mockStatic(Jsoup.class);
+    when(Jsoup.connect(anyString())).thenReturn(mock(Connection.class));
+    when(Jsoup.connect(anyString()).get()).thenReturn(doc);
+    when(doc.select(anyString())).thenReturn(elements);
+    when(elements.size()).thenReturn(3);
+    when(elements.get(0)).thenReturn(mock(Element.class));
+    when(elements.get(0).select(anyString())).thenReturn(elements);
+    when(elements.get(0).select(anyString()).text()).thenReturn("Title");
+    when(elements.get(0).select(anyString()).attr(anyString())).thenReturn("https://www.dn.no");
+    when(elements.get(0).select(anyString()).attr(anyString()).split(",")[0].split(" ")[0]).thenReturn("https://www.dn.no");
+    when(elements.get(0).select(anyString()).attr(anyString())).thenReturn("https://www.dn.no");
+    when(elements.get(0).select(anyString()).text()).thenReturn("Category");
+
+    // Act
+    List<NewsDto> newsDtoList = newsScrapingService.scrapeDN(0, 1);
+
+    // Assert
+    assertEquals(1, newsDtoList.size());
+
+    when(Jsoup.connect(anyString())).thenReturn(mock(Connection.class));
+    when(Jsoup.connect(anyString()).get()).thenThrow(new IOException());
+    assertThrows(RuntimeException.class, () -> newsScrapingService.scrapeDN(1, 3));
+  }
+}
--- src/test/java/edu/ntnu/idatt2106/sparesti/service/stock/StockServiceTest.java
+++ src/test/java/edu/ntnu/idatt2106/sparesti/service/stock/StockServiceTest.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2106.sparesti.service.stock;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ntnu.idatt2106.sparesti.dto.stock.StockDataDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,21 +9,37 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StockServiceTest {
 
-  @InjectMocks
-  StockService stockService;
-
   @Mock
   RestTemplate restTemplate;
 
+  @InjectMocks
+  StockService stockService;
+
+
+
   @Test
-  void Service_GetStockData_ReturnsCorrectStockData() {
-    //Arrange
+  void Service_GetStockData_ReturnsCorrectStockData() throws JsonProcessingException {
+
+//    String symbol = "AAPL";
+//    // Arrange
+//    String jsonResponse = "{\"ticker\":\"AAPL\",\"queryCount\":1,\"resultsCount\":1,\"adjusted\":true,\"results\":[{\"v\":6.4066593e+07,\"vw\":172.1915,\"o\":173.33,\"c\":170.33,\"h\":174.99,\"l\":170,\"t\":1714449600000,\"n\":647398}],\"status\":\"DELAYED\",\"request_id\":\"d242686a107f50766796da8042f84622\",\"count\":1}";
+//    when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn(jsonResponse);
+//
+//    // Act
+//    StockDataDto actualStockDataDto = stockService.getStockData(symbol);
+//    StockDataDto expectedStockDataDto = new StockDataDto(170.33, -1.73);
+//
+//    // Assert
+//    assertEquals(expectedStockDataDto, actualStockDataDto);
+//    assertEquals(expectedStockDataDto.getChangePercent(), actualStockDataDto.getChangePercent());
   }
+
 }
