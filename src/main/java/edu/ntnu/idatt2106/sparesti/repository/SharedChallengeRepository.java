8782a31070836eac061e8469bd036c19910cf002
tabir <tabirijeffrey@gmail.com>
2024-05-02 20:03:02 +0200

    chore(repo): add checkstyle, sonarlint, javadoc

--- src/main/java/edu/ntnu/idatt2106/sparesti/repository/SharedChallengeRepository.java
+++ src/main/java/edu/ntnu/idatt2106/sparesti/repository/SharedChallengeRepository.java
@@ -1,13 +1,23 @@
 package edu.ntnu.idatt2106.sparesti.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import edu.ntnu.idatt2106.sparesti.model.challenge.SharedChallenge;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
+/**
+ * Repository interface for managing shared challenges.
+ * The interface extends JpaRepository and allows basic
+ * CRUD (Create, Read, Update, Delete) operations.
+ *
+ * @author Jeffrey Yaw Annor Tabiri
+ * @version 1.0
+ * @see SharedChallenge
+ */
 @Repository
 public interface SharedChallengeRepository extends JpaRepository<SharedChallenge, Long> {
   List<SharedChallenge> findSharedChallengeBySharedChallengeCode_Id(long code);
+
   List<SharedChallenge> findSharedChallengeBySharedChallengeCode_JoinCode(String code);
 }
